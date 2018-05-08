fdcb514b63781d9f603287c108cf475385b7a2d2
NakanishiJun <j.nakanishi@gingerapp.co.jp>
2018-05-08 16:58:27 +0900

    create multiple list of form helper

--- Railsapp/app/controllers/hello_controller.rb
+++ Railsapp/app/controllers/hello_controller.rb
@@ -3,14 +3,18 @@ class HelloController < ApplicationController
   def index
     if request.post? then
       @title = 'Result'
-      if params['r1'] then
-        @msg = 'you selected: ' + params['r1']
+      if params['s1'] then
+        @msg = 'you selected: '
+        for val in params['s1']
+          @msg += val + ' '
+        end
+        
       else
         @msg = 'not selected'
       end
     else
       @title = 'Index'
-      @msg = 'Select radio button!'
+      @msg = 'Select list!'
     end
   end
 end
