71a12161a87b89674b9f1b5abfdbe17e9a316b93
NakanishiJun <j.nakanishi@gingerapp.co.jp>
2018-05-08 15:25:30 +0900

    create form

--- Railsapp/config/routes.rb
+++ Railsapp/config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   get 'hello/index'
   get 'hello', to: 'hello#index'
   get 'hello/other'
+
+  post 'hello', to: 'hello#index'
+  post 'hello/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
