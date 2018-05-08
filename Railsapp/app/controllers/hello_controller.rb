0e2382968d8104f606ce1caf8cf357ada411607e
NakanishiJun <j.nakanishi@gingerapp.co.jp>
2018-05-08 16:00:12 +0900

    create check box of form helper

--- Railsapp/app/controllers/hello_controller.rb
+++ Railsapp/app/controllers/hello_controller.rb
@@ -1,14 +1,16 @@
 class HelloController < ApplicationController
-  protect_from_forgery
+
   def index
     if request.post? then
       @title = 'Result'
-      @msg = 'you typed: ' + params['input1'] + '.'
-      @value = params['input1']
+      if params['check1'] then
+        @msg = 'you Checked!!'
+      else
+        @msg = 'not checked ... Fuuuuuuuuuuuuuuck!!!!!!!!!!'
+      end
     else
       @title = 'Index'
-      @msg = 'type text...'
-      @value = ''
+      @msg = 'check it!'
     end
   end
 end
