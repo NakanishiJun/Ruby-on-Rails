0a66ccd5cf5b62756f694503a0375ce43ed4c7ad
NakanishiJun <j.nakanishi@gingerapp.co.jp>
2018-05-08 12:11:21 +0900

    redirect section

--- Railsapp/config/routes.rb
+++ Railsapp/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'hello/index'
   get 'hello', to: 'hello#index'
+  get 'hello/other'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
