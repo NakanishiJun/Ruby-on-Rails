ef0e8699232ca7a9ce9b72d4c883a263935f3c59
NakanishiJun <>
2018-05-22 04:13:35 +0900

    work at home 2018-05-21

--- Railsapp/app/controllers/board_controller.rb
+++ Railsapp/app/controllers/board_controller.rb
@@ -1,4 +1,19 @@
 class BoardController < ApplicationController
+  layout 'board'
+  def initialize
+    sugar
+    begin
+        @board_data = JSON.parse(File.read("data.txt"))
+    rescue
+        @board_data = Hash.new
+    end
+
+    @board_data.each do |key,obj|
+      if Time.now.to_i - key.to_i > 24*60*60 then
+
+
+      end
+  end
   def index
   end
 end
