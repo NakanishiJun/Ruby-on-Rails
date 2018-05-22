32135466990b390434348af69936a0c64ffb2824
NakanishiJun <j.nakanishi@gingerapp.co.jp>
2018-05-22 16:22:16 +0900

    complete chapter 1

--- Railsapp/config/routes.rb
+++ Railsapp/config/routes.rb
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
   post 'hello', to: 'hello#index'
   post 'hello/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'board', to: 'board#index'
+  post 'board', to: 'board#index'
+  get 'board/index'
+  post 'board/index'
 end
