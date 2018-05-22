e276b5d6ea060d2f31f0cce52dc5ce9cba52320f
NakanishiJun <j.nakanishi@gingerapp.co.jp>
2018-05-22 14:30:47 +0900

    fixed

--- Railsapp/app/controllers/board_controller.rb
+++ Railsapp/app/controllers/board_controller.rb
@@ -1,7 +1,7 @@
 class BoardController < ApplicationController
   layout 'board'
   def initialize
-    sugar
+    super
     begin
         @board_data = JSON.parse(File.read("data.txt"))
     rescue
@@ -10,10 +10,30 @@ class BoardController < ApplicationController
 
     @board_data.each do |key,obj|
       if Time.now.to_i - key.to_i > 24*60*60 then
-
-
+        @board_data.delete(key)
       end
+    end
   end
   def index
+    if request.post? then
+      obj = MyData.new(msg:params['msg'], name:params['name'], mail:params['mail'])
+      @board_data[Time.now.to_i] = obj
+      data = @board_data.to_json
+      File.write("data.txt", data)
+      @board_data = JSON.parse(data)
+    end
+  end
+
+end
+
+class MyData
+  attr_accessor :name
+  attr_accessor :mail
+  attr_accessor :msg
+
+  def initialize msg:msg, name:name, mail:mail
+    self.name = name
+    self.mail = mail
+    self.msg = msg
   end
 end
--- book_admin/config/routes.rb
+++ book_admin/config/routes.rb
@@ -1,17 +1,7 @@
 Rails.application.routes.draw do
-  resource :profile, only: %i(show edit update)
-
-  get "/books/:id" => "books#show"
-  resource :publishers do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    resources :books
-
-    member do
-      get 'detail'
-    end
-
-    collection do
-      get 'search'
-    end
   end
+  get '/board', to: '/board#index'
+  post '/board', to: '/board#index'
+  get '/board/index'
+  post '/board/index'
 end
