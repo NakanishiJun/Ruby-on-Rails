3b63b6ce124878f663cf8f43321fb7e99b947b1a
NakanishiJun <j.nakanishi@gingerapp.co.jp>
2018-05-25 14:49:33 +0900

    不要な出力を削除及び機能の修正

--- Railsapp/config/routes.rb
+++ Railsapp/config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   get 'cards/index'
   get 'cards', to: 'cards#index'
 
-  get 'cards/:id', to: 'cards#show'
-
   get 'cards/add'
   post 'cards/add'
+
+  get 'cards/:id', to: 'cards#show'
 
   get 'cards/edit/:id', to: 'cards#edit'
   patch 'cards/edit/:id', to: 'cards#edit'
