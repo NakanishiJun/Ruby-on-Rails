e5d623be01193261f459cea34127bbcd02a28235
NakanishiJun <j.nakanishi@gingerapp.co.jp>
2018-05-25 16:45:35 +0900

    fixed to LIKE OR search

--- Railsapp/app/controllers/people_controller.rb
+++ Railsapp/app/controllers/people_controller.rb
@@ -37,8 +37,9 @@ class PeopleController < ApplicationController
     @msg = 'plese type under age'
     @people = Array.new
     if request.post? then
-      f = params[:find].split ','
-      @people = Person.where "age >= ? and age <= ?", f[0], f[1]
+      f = '%' + params[:find] + '%'
+      @people = Person.where "name like ? or mail like ?", f, f
+
     end
   end
 
