980b9dff771097ddef938b5313211eb7bff698ad
NakanishiJun <j.nakanishi@gingerapp.co.jp>
2018-06-01 16:11:50 +0900

    fixed something and change has_one to has_many

--- Railsapp/app/models/person.rb
+++ Railsapp/app/models/person.rb
@@ -1,5 +1,5 @@
 class Person < ApplicationRecord
-  has_one :message
+  has_many :message
   validates :name, presence: {message: 'は必須項目です'}
   validates :age, numericality: {message: 'は数字で入力してください'}
   validates :mail, email: {message: 'はメールアドレスではありません'}
