f33446c8a4611dbd02f86470ce3b71f79e8cad91
NakanishiJun <j.nakanishi@gingerapp.co.jp>
2018-06-08 12:17:43 +0900

    add validation

--- Railsapp/app/models/mycontact.rb
+++ Railsapp/app/models/mycontact.rb
@@ -1,2 +1,5 @@
 class Mycontact < ApplicationRecord
+  validates :name, presence: {message: 'は必須項目です。'}
+  validates :age, numericality: {message:'は数字で入力してください。'}
+  validates :nationality, inclusion: {in: [true, false], messag:'は真偽値です。'}
 end
